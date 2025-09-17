b75757b2e285a665bab29094f2b680fd66950e15
colo808 <149461288+colo808@users.noreply.github.com>
2025-09-17 14:56:34 +0200

    4294967295

--- engine2_dll.rs
+++ engine2_dll.rs
@@ -1,5 +1,5 @@
 // Generated using https://github.com/a2x/cs2-dumper
-// 2025-09-11 10:07:24.108570 UTC
+// 2025-09-17 12:54:51.154693500 UTC
 
 #![allow(non_upper_case_globals, non_camel_case_types, non_snake_case, unused)]
 
