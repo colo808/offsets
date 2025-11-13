53bb57d7a938575785163c015994f81f63eadacf
colo808 <149461288+colo808@users.noreply.github.com>
2025-11-13 15:58:58 +0100

    14123

--- client_dll.rs
+++ client_dll.rs
@@ -1,5 +1,5 @@
 // Generated using https://github.com/a2x/cs2-dumper
-// 2025-11-13 00:22:37.925935900 UTC
+// 2025-11-13 14:57:14.002794 UTC
 
 #![allow(non_upper_case_globals, non_camel_case_types, non_snake_case, unused)]
 
